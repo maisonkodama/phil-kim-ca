e17904f525a1484387c77674cc7c098567732cd5
Phil Kim <79614110+maisonkodama@users.noreply.github.com>
2021-04-29 19:43:31 -0400

    first add of js hover change text h1 and css for pointer cursor on hover for h1

---
+++ app.js
@@ -0,0 +1,7 @@
+let mainh1 = document.querySelector(".main__h1");
+mainh1.addEventListener("mouseover", function(){
+    mainh1.innerHTML = "Maison Kodama"
+  });
+  mainh1.addEventListener("mouseout", function (){
+    mainh1.innerHTML = "Phil Kim"
+  });
